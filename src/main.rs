0a3b842babae44d5b49fa3bf638e653a64516d92
Lowell Thoerner <lowellashton@gmail.com>
2023-01-24 11:34:47 -0800

    Initial commit, added support for a few conversion types.

---
+++ src/main.rs
@@ -0,0 +1,123 @@
+#![allow(dead_code)]
+
+use std::io::{stdin, stdout, Write};
+
+const RYDBERG: f64 = -2.178e-18;
+const PLANCK: f64 = 6.626e-34;
+const AVOGADRO: f64 = 6.022e23;
+const SPEED_OF_LIGHT: f64 = 2.998e8;
+
+fn main() {
+    // Give the user a menu of calculations to choose from
+    println!("Choose a calculation to perform:");
+    println!("1. Calculate energy release from electronic transition");
+    println!("2. Calculate threshold frequency from work function");
+    println!("3. Calculate work function from threshold frequency");
+    println!("4. Calculate wavelength from frequency");
+    println!("5. Calculate frequency from wavelength");
+
+    let mut choice = String::new();
+    stdin().read_line(&mut choice).expect("Failed to read line");
+    let choice: i32 = choice.trim().parse().expect("Please type a number!");
+
+    match choice {
+        1 => prompt_electronic_transition_energy(),
+        2 => prompt_threshold_frequency(),
+        3 => prompt_work_function(),
+        4 => prompt_wavelength_from_frequency(),
+        5 => prompt_frequency_from_wavelength(),
+        _ => println!("Invalid choice!"),
+    }
+}
+
+fn prompt_electronic_transition_energy() {
+    let n = read_i32("Enter the first energy level (n): ");
+    let m = read_i32("Enter the second energy level (m): ");
+    let significant_figures = read_usize("Enter the number of significant figures needed: ") - 1;
+
+    let energy_difference = calculate_electronic_transition_energy(n, m);
+
+    println!("The energy difference between the two energy levels is {:.1$e} J", energy_difference, significant_figures);
+}
+
+fn prompt_threshold_frequency() {
+    // ! Is this unit correct?
+    let work_function = read_f64("Enter the work function (in J/mol): ");
+    let significant_figures = read_usize("Enter the number of significant figures needed: ") - 1;
+
+    let threshold_frequency = calculate_threshold_frequency(work_function);
+
+    println!("The threshold frequency is {:.1$e} Hz", threshold_frequency, significant_figures);
+}
+
+fn prompt_work_function() {
+    let threshold_frequency = read_f64("Enter the threshold frequency (in Hz): ");
+    let significant_figures = read_usize("Enter the number of significant figures needed: ") - 1;
+
+    let work_function = calculate_work_function(threshold_frequency);
+
+    println!("The work function is {:.1$e} J/mol", work_function, significant_figures);
+}
+
+fn prompt_wavelength_from_frequency() {
+    let frequency = read_f64("Enter the frequency (in Hz): ");
+    let significant_figures = read_usize("Enter the number of significant figures needed: ") - 1;
+
+    let wavelength = calculate_wavelength_from_frequency(frequency);
+
+    println!("The wavelength is {:.1$e} m", wavelength, significant_figures);
+}
+
+fn prompt_frequency_from_wavelength() {
+    let wavelength = read_f64("Enter the wavelength (in m): ");
+    let significant_figures = read_usize("Enter the number of significant figures needed: ") - 1;
+
+    let frequency = calculate_frequency_from_wavelength(wavelength);
+
+    println!("The frequency is {:.1$e} Hz", frequency, significant_figures);
+}
+
+// Given two electron energy levels (n and m), calculates the energy released (in J)
+fn calculate_electronic_transition_energy(n: i32, m: i32) -> f64 {
+    RYDBERG * (1.0 / (n * n) as f64 - 1.0 / (m * m) as f64)
+}
+
+// Given a work function (in J/mol), calculates the threshold frequency of a photoelectric effect
+fn calculate_threshold_frequency(w: f64) -> f64 {
+    w / PLANCK
+}
+
+// Given a threshold frequency (in Hz), calculates the work function of a photoelectric effect
+fn calculate_work_function(f: f64) -> f64 {
+    f * PLANCK
+}
+
+// Given a frequency (in Hz), calculates the wavelength (in m)
+fn calculate_wavelength_from_frequency(f: f64) -> f64 {
+    SPEED_OF_LIGHT / f
+}
+
+// Given a wavelength (in m), calculates the frequency (in Hz)
+fn calculate_frequency_from_wavelength(l: f64) -> f64 {
+    SPEED_OF_LIGHT / l
+}
+
+fn read_i32(prompt: &str) -> i32 {
+    prompt_string(prompt).parse().expect("Please type a number!")
+}
+
+fn read_usize(prompt: &str) -> usize {
+    prompt_string(prompt).parse().expect("Please type a number!")
+}
+
+fn read_f64(prompt: &str) -> f64 {
+    prompt_string(prompt).parse().expect("Please type a number!")
+}
+
+fn prompt_string(prompt: &str) -> String {
+    print!("{}", prompt);
+    stdout().flush().expect("Failed to flush stdout");
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Failed to read line");
+    input.trim().to_string()
+}
