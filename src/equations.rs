a9fcdd4d751488a7acbe80f40f31cbbaf6b1ee42
Lowell Thoerner <lowellashton@gmail.com>
2023-01-24 14:17:31 -0800

    Added Greek unicode characters for frequency.

--- src/equations.rs
+++ src/equations.rs
@@ -1,5 +1,7 @@
 use crate::constants::{RYDBERG, PLANCK, SPEED_OF_LIGHT};
 
+// TODO: Use unicode names for variables
+
 // Given two electron energy levels (n and m), calculates the energy released (in J)
 pub fn calculate_electronic_transition_energy(n: i32, m: i32) -> f64 {
     RYDBERG * (1.0 / (n * n) as f64 - 1.0 / (m * m) as f64)
@@ -11,13 +13,13 @@ pub fn calculate_threshold_frequency(w: f64) -> f64 {
 }
 
 // Given a threshold frequency (in Hz), calculates the work function of a photoelectric effect
-pub fn calculate_work_function(f: f64) -> f64 {
-    f * PLANCK
+pub fn calculate_work_function(ν: f64) -> f64 {
+    ν * PLANCK
 }
 
 // Given a frequency (in Hz), calculates the wavelength (in m)
-pub fn calculate_wavelength_from_frequency(f: f64) -> f64 {
-    SPEED_OF_LIGHT / f
+pub fn calculate_wavelength_from_frequency(ν: f64) -> f64 {
+    SPEED_OF_LIGHT / ν
 }
 
 // Given a wavelength (in m), calculates the frequency (in Hz)
@@ -26,8 +28,8 @@ pub fn calculate_frequency_from_wavelength(λ: f64) -> f64 {
 }
 
 // Given a frequency (in Hz), calculates the energy (in J)
-pub fn calculate_energy_from_frequency(f: f64) -> f64 {
-    PLANCK * f
+pub fn calculate_energy_from_frequency(ν: f64) -> f64 {
+    PLANCK * ν
 }
 
 // Given a wavelength (in m), calculates the energy (in J)
